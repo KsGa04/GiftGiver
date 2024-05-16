63dffa7b2397854c214ffa2e54dfb41ba4e299fa
Ксения Гаранчева <GaranchevaKS@stud.kai.ru>
2024-05-16 20:27:00 +0300

    Корректировки

--- GiftGiver/wwwroot/js/private.js
+++ GiftGiver/wwwroot/js/private.js
@@ -1,1 +1,24 @@
-﻿
+﻿document.querySelectorAll('.delete-wish').forEach(btn => {
+    btn.addEventListener('click', function () {
+        var подарокId = this.getAttribute('data-подарокid');
+        fetch(`/Home/УдалитьЖелаемое?подарокId=${подарокId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка удаления желаемого');
+                }
+                else {
+                    alert('Товар удален');
+                    window.location.href = "/Home/PrivateAcc";
+                }
+                // Дополнительная обработка данных при необходимости
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    });
+});
