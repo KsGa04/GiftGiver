7cb40a6cea96d37485a41230a12ab88c0a7eff24
Ксения Гаранчева <GaranchevaKS@stud.kai.ru>
2024-06-09 15:53:28 +0300

    Тесты

---
+++ GiftGiver/tests/get_all_by_id_test.py
@@ -0,0 +1,36 @@
+import json
+import unittest
+import requests
+
+
+class TestAllProductsController(unittest.TestCase):
+    def setUp(self):
+        self.base_url = "https://localhost:7276"
+        self.route = "/allproducts"
+
+    def test_get_all_products_by_id(self):
+        user_id = 1
+
+        response = requests.get(f"{self.base_url}{self.route}/{user_id}", verify=False)
+
+        self.assertEqual(response.status_code, 200)
+
+        products = response.json()
+        self.assertIsInstance(products, list)
+
+        self.assertTrue(len(products) > 0)
+
+        for product in products:
+            self.assertIsInstance(product, dict)
+            self.assertIn("подаркиId", product)
+            self.assertIn("наименование", product)
+
+    def test_get_all_products(self):
+        response = requests.get(f"{self.base_url}{self.route}", verify=False)
+
+        self.assertEqual(response.status_code, 200)
+
+        products = response.json()
+        self.assertIsInstance(products, list)
+
+        self.assertTrue(len(products) > 0)
